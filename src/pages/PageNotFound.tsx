ba754f3fe11d5a69df7d9f2bd77509e984de827e
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-22 00:01:33 +0200

    Created page not found

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { FullscreenPage } from "@/components/ui/FullscreenPage";
+import { Button } from "@/components/ui/button";
+import { MoveLeft } from "lucide-react";
+
 function PageNotFound() {
-  return <div>Error 404 Page Not Found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <FullscreenPage>
+      <div>
+        <h1 className="text-5xl font-semibold text-slate-800">404 - Not Found</h1>
+        <p className="mb-12 mt-5 text-slate-500">
+          Sorry the page you're looking for does not exist.
+        </p>
+        <Button
+          variant={"outline"}
+          size={"lg"}
+          className="w-full"
+          onClick={() => navigate(-1)}
+        >
+          <MoveLeft className="mr-2" />
+          <span>Go back</span>
+        </Button>
+      </div>
+    </FullscreenPage>
+  );
 }
 
 export default PageNotFound;
