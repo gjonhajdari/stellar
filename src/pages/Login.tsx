344eadf42e06db50ae4a6cfb6e9c656b1df3aafa
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-22 17:24:58 +0200

    Added basic login functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,8 +11,22 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import { MoveRight } from "lucide-react";
 import { FullscreenPage } from "@/components/ui/FullscreenPage";
+import { SyntheticEvent, useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState<string>("gjonhajdari1@gmail.com");
+  const [password, setPassword] = useState<string>("12345");
+  const navigate = useNavigate();
+
+  function handleLogin(e: SyntheticEvent): void {
+    e.preventDefault();
+
+    if (!email || !password) return;
+
+    if (email === "gjonhajdari1@gmail.com" && password === "12345")
+      navigate("/dashboard");
+  }
+
   return (
     <FullscreenPage>
       <Card className="w-[400px]">
@@ -26,21 +40,35 @@ function Login() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <div className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input type="email" placeholder="Email address" id="email" />
+          <form onSubmit={handleLogin}>
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  type="email"
+                  placeholder="Email address"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
             </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input type="password" placeholder="Password" id="password" />
-            </div>
-          </div>
 
-          <Button size={"lg"} className="mt-6 w-full">
-            <span className="mr-3">Log in</span>
-            <MoveRight />
-          </Button>
+            <Button size={"lg"} className="mt-6 w-full">
+              <span className="mr-3">Log in</span>
+              <MoveRight />
+            </Button>
+          </form>
         </CardContent>
       </Card>
     </FullscreenPage>
