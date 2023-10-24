0da33df9ecf0b88da1360b023197bb75dc7e7e82
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-24 23:45:00 +0200

    Created header for employee page

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,5 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { FileDown, HelpCircle, PlusCircle } from "lucide-react";
+
 function Employees() {
-  return <div>Employees</div>;
+  return (
+    <div className="w-full">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <h1 className="text-3xl">Employees</h1>
+          <HelpCircle size={20} className="text-slate-400" />
+          <span className="rounded-full bg-pink-800/5 px-2 py-1 text-sm text-pink-800">
+            100
+          </span>
+        </div>
+
+        <div className="space-x-3">
+          <Button variant={"outline"}>
+            <FileDown strokeWidth={1.5} size={20} className="mr-2" />
+            <span>Export</span>
+          </Button>
+
+          <Button>
+            <PlusCircle strokeWidth={1.5} size={20} className="mr-2" />
+            <span>New Employee</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Employees;
